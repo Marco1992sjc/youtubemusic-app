e5772839be52db95efb461d7b596de58f8816ea3
Marco1992sjc <marco1992sjc@gmail.com>
2021-04-16 16:46:28 -0300

    botoes de midia
    adicionado os botoes de play/next/previous, por enquanto estão sem funcionalidade, preiso estuar como setar para interagir com a música

--- main.js
+++ main.js
@@ -2,6 +2,9 @@
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
 
+
+
+
 function createWindow () {
 
   const mainWindow = new BrowserWindow({
@@ -18,9 +21,27 @@ function createWindow () {
   mainWindow.setMenuBarVisibility(false)
   mainWindow.loadURL('https://music.youtube.com/', {userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) old-airport-include/1.0.0 Chrome Electron/7.1.7 Safari/537.36'})
 
+ 
   
-
-
+  mainWindow.setThumbarButtons([
+  
+    {
+      tooltip: 'button1',
+      icon: path.join(__dirname, 'assets/taskbar/faixaanterior.png'),
+      click () { console.log('button1 clicked') }
+    },
+    {
+      tooltip: 'button2',
+      icon: path.join(__dirname, 'assets/taskbar/play-pause.png'),
+      click () { console.log('button1 clicked') }
+    },
+    {
+      tooltip: 'button3',
+      icon: path.join(__dirname, 'assets/taskbar/proximafaixa.png'),
+      click () { console.log('button1 clicked') }
+    }
+  ])
+  
 
 
 }
@@ -39,4 +60,7 @@ app.whenReady().then(() => {
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 })
+
+
+
 
